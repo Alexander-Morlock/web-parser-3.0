23ee89a1a9c901a25f7adf134d81371dd068ab59
Alexander Morlock <artofdesign@inbox.ru>
2024-11-16 19:41:35 +0100

    job is done promise-based refactoring

--- src/utils/parse.ts
+++ src/utils/parse.ts
@@ -1,6 +1,6 @@
 import { delay, downloadImage, generateImagePath, saveTexts } from "./utils"
 import { JSDOM } from "jsdom"
-import { ParserConfig } from "./types"
+import { ImageParserConfig, ParserConfig } from "./types"
 
 export function parse({
   domainName,
@@ -18,59 +18,76 @@ export function parse({
   const textsHeaderRow = textParsers?.map(({ name }) => name).join("\t")
   textsHeaderRow && texts.push(`ART\tURL\t${textsHeaderRow}\n`)
 
-  productCodes.forEach(async (productCode, productCodesIterationIndex) => {
-    {
-      await delay(requestDelay)
+  Promise.all(
+    productCodes.map(async (productCode, productCodesIterationIndex) => {
+      {
+        await delay(requestDelay)
 
-      try {
-        const productUrl = productUrls[productCodes.indexOf(productCode)]
-        const response = await fetch(encodeURI(productUrl))
-        const text = await response.text()
-        const html = new JSDOM(text).window.document
+        try {
+          const productUrl = productUrls[productCodes.indexOf(productCode)]
+          const response = await fetch(encodeURI(productUrl))
+          const text = await response.text()
+          const html = new JSDOM(text).window.document
 
-        // Downloading images from parsed urls
-        imageParsers?.forEach(
-          async ({ selector, source, maxNumberOfImages }) => {
-            const nodesToExtractImageUrl = Array.from(
-              html.querySelectorAll(selector)
-            )
-            const parsedUrls = nodesToExtractImageUrl
-              .map((node) => String(node[source as keyof Element]))
-              .slice(0, maxNumberOfImages)
-
-            parsedUrls.forEach(async (parsedUrl, index) => {
-              const url = parsedUrl.includes(domainName)
-                ? parsedUrl
-                : `${domainName}/${parsedUrl}`.replace(/\/+/g, "/")
-              const path = generateImagePath(folderName, productCode, index)
-
-              await downloadImage(path, url)
-            })
-          }
-        )
-
-        // Parsing texts
-        const textRow: string | undefined = textParsers
-          ?.map(({ selector }) =>
-            Array.from(html.querySelectorAll(selector)) // nodes to parse
-              .map((node) => node.textContent?.replace(/[\t\n]/g, ""))
-              .join("\t")
+          await downloadImages(
+            imageParsers,
+            html,
+            domainName,
+            folderName,
+            productCode
           )
-          .join("\t")
 
-        texts.push(`${productCode}\t${productUrl}\t${textRow}`)
+          // Parsing texts
+          const textRow: string | undefined = textParsers
+            ?.map(({ selector }) =>
+              Array.from(html.querySelectorAll(selector)) // nodes to parse
+                .map((node) => node.textContent?.replace(/[\t\n]/g, ""))
+                .join("\t")
+            )
+            .join("\t")
 
-        const isLastIteration =
-          productCodesIterationIndex === productCodes.length - 1
+          texts.push(`${productCode}\t${productUrl}\t${textRow}`)
 
-        if (!isLastIteration || !textParsers) {
-          return
+          const isLastIteration =
+            productCodesIterationIndex === productCodes.length - 1
+
+          if (!isLastIteration || !textParsers) {
+            return
+          }
+
+          await saveTexts("texts.txt", folderName, texts.join("\n"))
+        } catch (error) {
+          console.log("ERROR -> ", error)
         }
-
-        await saveTexts("texts.txt", folderName, texts.join("\n"))
-      } catch (error) {
-        console.log("ERROR -> ", error)
       }
-    }
+    })
+  ).then(() => console.log("-----------------> JOB IS DONE <-----------------"))
+}
+
+/** Downloading images from parsed urls */
+function downloadImages(
+  imageParsers: ImageParserConfig<HTMLElement>[] | undefined,
+  html: Document,
+  domainName: string,
+  folderName: string,
+  productCode: string
+) {
+  return new Promise((resolve) => {
+    imageParsers?.forEach(async ({ selector, source, maxNumberOfImages }) => {
+      const nodesToExtractImageUrl = Array.from(html.querySelectorAll(selector))
+      const parsedUrls = nodesToExtractImageUrl
+        .map((node) => String(node[source as keyof Element]))
+        .slice(0, maxNumberOfImages)
+
+      Promise.all(
+        parsedUrls.map((parsedUrl, index) => {
+          const url = parsedUrl.includes(domainName)
+            ? parsedUrl
+            : `${domainName}/${parsedUrl}`.replace(/\/+/g, "/")
+          const path = generateImagePath(folderName, productCode, index)
+          return downloadImage(path, url)
+        })
+      ).then(resolve)
+    })
   })
 }
